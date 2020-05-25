d5b5694c9d0c8603339d74850fce21c3a8de638c
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-05-24 20:48:57 -0700

    Improve frontend

--- src/view/app.ts
+++ src/view/app.ts
@@ -1,9 +1,11 @@
 import { element } from "boredom";
-import { NotesApp } from "./view.js";
+import { NotesApp } from "./view";
 
 const root = document.getElementById("root");
 const app = element.create(NotesApp);
 
 if (root) {
   element.mount(root, app);
+} else {
+  console.error("cannot create app");
 }
