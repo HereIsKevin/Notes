63e36cd44de00054e348a065511d7d6614551909
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-05-27 21:05:31 -0700

    Create asynchronous fsutils

---
+++ src/fsutils.ts
@@ -0,0 +1,53 @@
+export { exists, createDirectory, rename, writeFile, readFile };
+
+import { promises as fs } from "fs";
+import { constants as fsConstants } from "fs";
+
+import * as logger from "./logger";
+
+async function exists(path: string): Promise<boolean> {
+  try {
+    await fs.access(path, fsConstants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function createDirectory(path: string): Promise<void> {
+  try {
+    await fs.mkdir(path, { recursive: true });
+    logger.log(`created directories to ${path}`);
+  } catch {
+    logger.error(`failed to create directories to ${path}`);
+  }
+}
+
+async function rename(oldPath: string, newPath: string): Promise<void> {
+  try {
+    await fs.rename(oldPath, newPath);
+    logger.log(`renamed ${oldPath} to ${newPath}`);
+  } catch {
+    logger.error(`failed to rename ${oldPath} to ${newPath}`);
+  }
+}
+
+async function writeFile(path: string, content: string): Promise<void> {
+  try {
+    await fs.writeFile(path, content);
+    logger.log(`wrote ${path}`);
+  } catch {
+    logger.error(`failed to write ${path}`);
+  }
+}
+
+async function readFile(path: string): Promise<string | undefined> {
+  try {
+    const content: Buffer = await fs.readFile(path);
+    logger.log(`read ${path}`);
+    return content.toString();
+  } catch {
+    logger.error(`failed to read ${path}`);
+    return undefined;
+  }
+}
