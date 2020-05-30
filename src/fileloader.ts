6b56f36a1d056ed1fc8a44031e5627384b1aab73
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-05-29 17:09:01 -0700

    Improve FileLoader

--- src/fileloader.ts
+++ src/fileloader.ts
@@ -7,25 +7,28 @@ import * as path from "path";
 import { JSONDatabase } from "./jsondatabase";
 import * as fsutils from "./fsutils";
 
-const notesPath = [os.homedir(), ".notes"].join(path.sep);
-const fileDatabase = [notesPath, "files.json"].join(path.sep);
-const noteUnamed = "Untitled Note";
+const notesPath: string = [os.homedir(), ".notes"].join(path.sep);
+const fileDatabase: string = [notesPath, "files.json"].join(path.sep);
+const noteUnamed: string = "Untitled Note";
 
 class FileLoader {
   private static loaders: FileLoader[] = [];
   private static database: JSONDatabase;
 
   public file: string;
+  public contents: string;
 
   public constructor(file: string) {
     this.file = file;
+    this.contents = "";
   }
 
   public async open(): Promise<void> {
     const contents: string | undefined = await fsutils.readFile(this.file);
+    this.contents = contents || "";
 
-    for (let window of BrowserWindow.getAllWindows()) {
-      window.webContents.send("notes-contents", this.file, contents || "");
+    for (const window of BrowserWindow.getAllWindows()) {
+      window.webContents.send("notes-contents", this.file, this.contents);
       window.webContents.send("notes-open-finished");
     }
   }
@@ -34,7 +37,8 @@ class FileLoader {
     const title: string = contents.trim().split("\n")[0] || "Untitled Note";
     const oldPath: string = this.file;
     const newPath: string = await FileLoader.generateName(
-      [notesPath, `${title}.md`].join(path.sep)
+      [notesPath, `${title}.md`].join(path.sep),
+      (this.contents.trim().split("\n")[0] || "Untitled Note") === title
     );
 
     if (!(await fsutils.exists(notesPath))) {
@@ -45,12 +49,13 @@ class FileLoader {
     await fsutils.rename(oldPath, newPath);
 
     this.file = newPath;
+    this.contents = contents;
 
     delete FileLoader.database.json[oldPath];
     FileLoader.database.json[newPath] = title;
     FileLoader.database.write();
 
-    for (let window of BrowserWindow.getAllWindows()) {
+    for (const window of BrowserWindow.getAllWindows()) {
       window.webContents.send("notes-rename", oldPath, newPath, title);
       window.webContents.send("notes-save-finished");
     }
@@ -66,8 +71,8 @@ class FileLoader {
     FileLoader.database.json[this.file] = noteUnamed;
     FileLoader.database.write();
 
-    for (let window of BrowserWindow.getAllWindows()) {
-      window.webContents.send("notes-add", this.file, noteUnamed);
+    for (const window of BrowserWindow.getAllWindows()) {
+      window.webContents.send("notes-add", this.file);
       window.webContents.send("notes-new-finished");
     }
   }
@@ -75,34 +80,49 @@ class FileLoader {
   public async close(contents: string): Promise<void> {
     await this.save(contents);
 
-    for (let window of BrowserWindow.getAllWindows()) {
+    for (const window of BrowserWindow.getAllWindows()) {
       window.webContents.send("notes-close-finished");
     }
   }
 
-  public static async generateName(file: string): Promise<string> {
-    let fileName: string = file;
+  public static async generateName(
+    file: string,
+    original: boolean = false
+  ): Promise<string> {
+    let index: number = 0;
 
-    while (await fsutils.exists(fileName)) {
-      fileName = `${path.dirname(fileName)}${path.sep}${path.basename(
-        fileName,
-        path.extname(fileName)
-      )}_${path.extname(fileName)}`;
+    while (
+      await fsutils.exists(
+        index
+          ? `${path.dirname(file)}${path.sep}${path.basename(
+              file,
+              path.extname(file)
+            )}${index}${path.extname(file)}`
+          : file
+      )
+    ) {
+      index++;
     }
 
-    return fileName;
+    if (original) {
+      index--;
+      index = index < 0 ? 0 : index;
+    }
+
+    return index
+      ? `${path.dirname(file)}${path.sep}${path.basename(
+          file,
+          path.extname(file)
+        )}${index}${path.extname(file)}`
+      : file;
   }
 
   public static async initialize(): Promise<void> {
-    if (!(await fsutils.exists(notesPath))) {
-      await fsutils.createDirectory(notesPath);
-    }
-
     FileLoader.database = new JSONDatabase(fileDatabase);
     await FileLoader.database.read();
 
-    ipcMain.on("notes-open", (event: IpcMainEvent, file: string): void => {
-      const loader: FileLoader = new FileLoader(file);
+    ipcMain.on("notes-open", (event: IpcMainEvent, file: string) => {
+      const loader = new FileLoader(file);
       FileLoader.loaders.push(new FileLoader(file));
       loader.open();
     });
@@ -124,27 +144,30 @@ class FileLoader {
       }
     );
 
-    ipcMain.on("notes-close", (event: IpcMainEvent, file: string): void => {
-      for (let loader of FileLoader.loaders.filter(
-        (loader: FileLoader) => loader.file === file
-      )) {
-        // loader.close(); // enable after fixing frontend
-        FileLoader.loaders.splice(FileLoader.loaders.indexOf(loader), 1);
-        console.log(`[log] closed ${file}`);
+    ipcMain.on(
+      "notes-close",
+      (event: IpcMainEvent, file: string) => {
+        for (const loader of FileLoader.loaders.filter(
+          (loader: FileLoader) => loader.file === file
+        )) {
+          FileLoader.loaders.splice(FileLoader.loaders.indexOf(loader), 1);
+          console.log(`[log] closed ${file}`);
+        }
       }
+    );
+
+    ipcMain.on("notes-new", (event: IpcMainEvent) => {
+      FileLoader.generateName(
+        [notesPath, `${noteUnamed}.md`].join(path.sep)
+      ).then((value: string) => {
+        const loader = new FileLoader(value);
+        FileLoader.loaders.push(loader);
+        loader.new();
+      });
     });
 
-    ipcMain.on("notes-new", (event: IpcMainEvent): void => {
-      FileLoader.generateName([notesPath, `${noteUnamed}.md`].join(path.sep))
-        .then((value: string) => {
-          const loader: FileLoader = new FileLoader(value);
-          FileLoader.loaders.push(loader);
-          loader.new();
-        })
-    });
-
-    ipcMain.on("notes-load", (event: IpcMainEvent): void => {
-      for (let window of BrowserWindow.getAllWindows()) {
+    ipcMain.on("notes-load", (event: IpcMainEvent) => {
+      for (const window of BrowserWindow.getAllWindows()) {
         window.webContents.send(
           "notes-items",
           Object.entries(FileLoader.database.json)
