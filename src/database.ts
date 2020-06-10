1cd8486ff7477c886ea48b3a9e82499b0aa38a0a
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-06-10 12:26:34 -0700

    Add file functions to database along with more

--- src/database.ts
+++ src/database.ts
@@ -1,41 +1,96 @@
-export { JSONArray, JSONDatabase, JSONObject };
-
-import * as path from "path";
+export { Database, INotesDatabase };
 
 import * as fsutils from "./fsutils";
+import * as path from "path";
 
-type JSONArray = (string | number | boolean | JSONObject | JSONArray)[];
-type JSONObject = {
-  [key: string]: string | number | boolean | JSONObject | JSONArray;
+interface INotesDatabase {
+  files: [string, string][];
+}
+
+const emptyDatabase: INotesDatabase = {
+  files: [],
 };
 
-class JSONDatabase {
+class Database {
   private file: string;
 
-  public json: JSONObject;
+  public json: INotesDatabase;
 
   public constructor(file: string) {
     this.file = file;
-    this.json = {};
+    this.json = Object.assign({}, emptyDatabase);
   }
 
   public async read(): Promise<void> {
+    // create directory of database if it does not exist
     if (!(await fsutils.exists(path.dirname(this.file)))) {
       await fsutils.createDirectory(path.dirname(this.file));
     }
 
+    // write default database if the files does not exist
     if (!(await fsutils.exists(this.file))) {
-      await fsutils.writeFile(this.file, "{}");
+      await fsutils.writeFile(this.file, JSON.stringify(emptyDatabase));
     }
 
-    this.json = JSON.parse((await fsutils.readFile(this.file)) || "{}");
+    // parse the new database
+    const json = JSON.parse((await fsutils.readFile(this.file)) || "{}");
+
+    // assign parsed if it is not empty
+    if (Object.keys(json).length > 0 && json.constructor === Object) {
+      this.json = json;
+    }
   }
 
   public async write(): Promise<void> {
+    // create directory of database if it does not exist
     if (!(await fsutils.exists(path.dirname(this.file)))) {
       await fsutils.createDirectory(path.dirname(this.file));
     }
 
-    await fsutils.writeFile(this.file, JSON.stringify(this.json));
+    // write json to file
+    await fsutils.writeFile(this.file, JSON.stringify(this.json, null, "  "));
+  }
+
+  public renameFile(oldPath: string, newPath: string, title: string): void {
+    const files = this.json.files.filter(
+      (x: [string, string]) => x[0] === oldPath
+    );
+
+    let first = true;
+
+    for (const file of files) {
+      const index = this.json.files.indexOf(file);
+
+      if (index !== -1) {
+        this.json.files.splice(index, 1);
+
+        if (first) {
+          this.json.files.splice(index, 0, [newPath, title]);
+          first = false;
+        }
+      }
+    }
+
+    if (first) {
+      this.json.files.push([newPath, title]);
+    }
+  }
+
+  public deleteFile(oldPath: string): void {
+    const files = this.json.files.filter(
+      (x: [string, string]) => x[0] === oldPath
+    );
+
+    for (const file of files) {
+      const index = this.json.files.indexOf(file);
+
+      if (index !== -1) {
+        this.json.files.splice(index, 1);
+      }
+    }
+  }
+
+  public newFile(newPath: string, title: string): void {
+    this.json.files.push([newPath, title]);
   }
 }
